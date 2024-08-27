66fa88274144a6b9085a2c56591ec6da68165470
Soil King <157099073+soilking@users.noreply.github.com>
2024-08-26 19:01:25 -0700

    rate limit error instead of status check on error endpoints

--- src/services/subgraph-proxy-service.js
+++ src/services/subgraph-proxy-service.js
@@ -121,7 +121,23 @@ class SubgraphProxyService {
 
   // Throws an exception based on the failure reason
   static async _throwFailureReason(subgraphName, errors, failedEndpoints, unsyncdEndpoints) {
-    if (failedEndpoints.length > 0) {
+    if (failedEndpoints.length + unsyncdEndpoints.length < EnvUtil.endpointsForSubgraph(subgraphName).length) {
+      // If any of the endpoints were not attempted, assume this is a rate limiting issue.
+      // This is preferable to performing the status check on a failing endpoint,
+      // while another endpoint is presumably alive and actively servicing requests.
+      if (failedEndpoints.length + unsyncdEndpoints.length === 0) {
+        DiscordUtil.sendWebhookMessage(
+          `Rate limit exceeded on all endpoints for '${subgraphName}'. No endpoints attempted to service this request.`
+        );
+      } else {
+        DiscordUtil.sendWebhookMessage(
+          `Rate limit exceeded on endpoint(s) for '${subgraphName}'. At least one endpoint tried and failed this request.`
+        );
+      }
+      throw new RateLimitError(
+        'The server is currently experiencing high traffic and cannot process your request. Please try again later.'
+      );
+    } else if (failedEndpoints.length > 0) {
       if (new Date() - SubgraphState.getLatestSubgraphErrorCheck(subgraphName) < 60 * 1000) {
         if (SubgraphState.allHaveErrors(subgraphName)) {
           throw new EndpointError('Subgraph is unable to process this request and may be offline.');
@@ -173,14 +189,6 @@ class SubgraphProxyService {
       }
     } else if (unsyncdEndpoints.length > 0) {
       throw new EndpointError('Subgraph has not yet indexed up to the latest block.');
-    } else {
-      // No endpoint was even attempted
-      DiscordUtil.sendWebhookMessage(
-        `Rate limit exceeded on all endpoints for '${subgraphName}'. A request was dropped.`
-      );
-      throw new RateLimitError(
-        'The server is currently experiencing high traffic and cannot process your request. Please try again later.'
-      );
     }
   }
 }
